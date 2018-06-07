ae0d116614af957bdf2fc50ebdf10dfa4733ab1d
Max Blaushild <blaushmild@gmail.com>
2018-06-07 11:14:25 -0700

    Back to square one

--- index.js
+++
@@ -1,9 +0,0 @@
-const TruffleContract = require('truffle-contract')
-const server = require('./server')
-
-// load config
-require('./config')
-// start jobs
-require('./jobs/watch-record-add')
-
-server.start()
--- scripts/start_test_db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-docker run --name postgresql -it \
-  --publish 5432:5432 \
-  --rm \
-  --env 'PG_PASSWORD=postgres' \
-sameersbn/postgresql:9.6-2
