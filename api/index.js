ea8d4a502d1bd0c8f7ab96ba7f67422fa0adf3c6
Vicente Rotman <vrotmanh@gmail.com>
2018-06-22 14:20:44 -0400

    Set CORS

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ const app = express();
 const port = process.env.LINNIA_PORT;
 const bodyParser = require('body-parser');
 const errorHandler = require('./errorHandler');
+const cors = require('cors');
 
 const initialize = () => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
 
   app.get('/records', require('./records'));
   app.get('/users/:userAddress/permissioned-records', require('./recordsByPermission'));
