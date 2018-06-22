c74f98844f8b60e81739096871321cafedcdd0d7
MaxBlaushild <blaushmild@gmail.com>
2018-06-22 15:20:28 -0400

    makes sync togglable and off for testing

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ const api = require('./api');
 const sync = require('./sync');
 const linnia = require('./services/linnia');
 
+const isTesting = process.env.LINNIA_IS_TESTING;
+const shouldNotSync = process.env.LINNIA_SHOULD_NOT_SYNC;
+const shouldSync = !isTesting && !shouldNotSync;
+
 api.initialize();
 
-linnia.initialize().then(_linnia => sync.initialize(_linnia));
+if (shouldSync) {
+  linnia.initialize().then(_linnia => sync.initialize(_linnia));
+}
 
 
