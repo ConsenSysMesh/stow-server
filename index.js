3481069bd1239fc1e5eac33a5054959f667e4108
MaxBlaushild <blaushmild@gmail.com>
2018-06-26 11:28:19 -0400

    Always syncs models with database before initializing

--- index.js
+++ index.js
@@ -3,15 +3,19 @@ require('dotenv').config();
 const api = require('./api');
 const sync = require('./sync');
 const linnia = require('./services/linnia');
+const models = require('./models');
 
 const isTesting = process.env.LINNIA_IS_TESTING;
 const shouldNotSync = process.env.LINNIA_SHOULD_NOT_SYNC;
 const shouldSync = !isTesting && !shouldNotSync;
 
-api.initialize();
+models.initialize().then(() => {
+  api.initialize();
 
-if (shouldSync) {
-  linnia.initialize().then(_linnia => sync.initialize(_linnia));
-}
+  if (shouldSync) {
+    linnia.initialize().then(_linnia => sync.initialize(_linnia));
+  }
+});
+
 
 
