30f5e18787db7acb067be0b10a502ec79accbd94
Vicente Rotman <vrotmanh@gmail.com>
2018-07-24 11:07:04 -0400

    Search by address does not require lowercase

--- api/records.js
+++ api/records.js
@@ -3,6 +3,11 @@ const Sequelize = require('sequelize');
 
 module.exports = (req, res, next) => {
 
+  //Lowercase Owner address
+  if(req.query['owner']){
+    req.query['owner'] = req.query['owner'].toLowerCase();
+  }
+
   //If the request contains owner and property
   if(req.query['owner'] && req.query['property']){
     Record.findAll({ where: { owner: { [Sequelize.Op.eq]: req.query['owner'],},
