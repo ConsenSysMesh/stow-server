940d05e7b1a0cfe47b17e94daa6261a663d669c9
Vicente Rotman <vrotmanh@gmail.com>
2018-09-04 22:51:27 -0400

    Fix Sync Server

--- sync/syncPastEvents.js
+++ sync/syncPastEvents.js
@@ -5,9 +5,6 @@ const {
   serializePermission
 } = require('./serialization');
 
-const firstBlock = 0;
-const latestBlock = 'latest';
-
 module.exports = (linnia) => {
   const {
     LinniaRecordAdded,
@@ -23,45 +20,105 @@ module.exports = (linnia) => {
   .catch(panic);
 };
 
+let totalCalls = 0
+let callsDone = 0
+
 const getPastEvents = (event) => {
-  return new Promise((resolve, reject) => {
-    return event({}, {
-      fromBlock: firstBlock,
-      toBlock: latestBlock
-    }).get((err, events) => {
-      err ? reject(err) : resolve(events);
-    });
-  });
+  let results = []
+  let step = 50000
+  let firstBlock = 0
+  let endBlock = 3800000
+
+  while(endBlock < 4000000){
+    totalCalls+=1
+    results.push(
+      new Promise((resolve, reject) => {
+        return event({}, {
+          fromBlock: firstBlock,
+          toBlock: endBlock
+        }).get((err, events) => {
+          callsDone+=1
+          console.log("Sync done: " + callsDone + " out of :" +  totalCalls)
+          err ? reject(err) : resolve(events);
+        });
+      })
+    );
+    firstBlock = endBlock
+    endBlock += step
+  }
+  return results
 };
 
 const syncPastRecords = (recordsEvent, linnia) => {
-  return getPastEvents(recordsEvent).then(events => {
+  return Promise.all(getPastEvents(recordsEvent)).then(eventsArrays => {
+    let events = [].concat.apply([], eventsArrays);
+    let totalRecords = events.length;
+    let count = 0;
+    console.log("Storing " + totalRecords + " records")
     return Promise.all(events.map((event) => {
       return linnia.getRecord(event.args.dataHash)
         .then(record => serializeRecord(event, record))
-        .then(record => Record.findOrCreate({
-          where: record
-        }));
+        .then(record => {
+
+          // Show logs
+          count+=1;
+          if(count%20 == 0){
+            console.log("Stored " + count + " records, out of: " + totalRecords);
+          }
+
+          // Add record to DB
+          Record.findOrCreate({
+            where: record
+          })
+        }
+      );
     }));
   });
 };
 
 const syncPastPermissions = (permissionsEvent, linnia) => {
-  return getPastEvents(permissionsEvent).then(events => {
+  return Promise.all(getPastEvents(permissionsEvent)).then(eventsArrays => {
+    let events = [].concat.apply([], eventsArrays);
+    let totalPermissions = events.length;
+    let count = 0;
+    console.log("Storing " + totalPermissions + " permissions")
     return Promise.all(events.map((event) => {
       return linnia.getPermission(event.args.dataHash, event.args.viewer)
         .then(per => serializePermission(event, per))
-        .then(permission => permission.canAccess && Permission.findOrCreate({
-          where: permission
-        }));
+        .then(permission => {
+
+          // Show logs
+          count+=1;
+          if(count%20 == 0){
+            console.log("Stored " + count + " permissions, out of: " + totalPermissions);
+          }
+
+          // Add permission to DB
+          permission.canAccess && Permission.findOrCreate({
+            where: permission
+          })}
+        );
     }));
   });
 };
 
 const syncPastUsers = (usersEvent) => {
-  return getPastEvents(usersEvent)
-    .then(events => events.map(serializeUser))
+  let totalUsers, count;
+  return Promise.all(getPastEvents(usersEvent)).then(eventsArrays => {
+      let events = [].concat.apply([], eventsArrays);
+      totalUsers = events.length;
+      count = 0;
+      console.log("Storing " + totalUsers + " users")
+      return events.map(serializeUser)
+    })
     .then(users => Promise.all(users.map((user) => {
+      // Show logs
+      count+=1;
+      if(count%20 == 0){
+        console.log("Stored " + count + " users, out of: " + totalUsers);
+      }
+
+      // Add users to DB
       return User.findOrCreate({ where: user });
     })));
 };
@@ -71,5 +128,3 @@ const panic = (err) => {
   console.error(err);
   process.exit(1);
 };
-
-
