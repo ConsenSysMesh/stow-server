f2da6054c0035761c8d3f2734c64abd75bcf7586
Max Blaushild <blaushmild@gmail.com>
2018-10-01 09:03:52 -0400

    bug/doFirstIteration (#35)
    * bug/doFirstIteration
    
    * removes console log

--- sync/syncPastEvents.js
+++ sync/syncPastEvents.js
@@ -36,7 +36,7 @@ const getPastEvents = (event, blockNumber) => {
   let fromBlock = 0;
   let toBlock = 3800000;
 
-  while (toBlock < blockNumber + step) {
+  while (fromBlock < blockNumber + step) {
     results.push(
       new Promise((resolve, reject) => {
         return event({}, {
